3f15ddb004ca930b546949f5d86c7c9ebb1e76fc
贺治东 <16471114+arronai@user.noreply.gitee.com>
2026-06-03 10:10:51 +0800

    Update to v1.0.1

--- Casks/ailernhub.rb
+++ Casks/ailernhub.rb
@@ -1,6 +1,6 @@
 cask "ailernhub" do
-  version "1.0.0"
-  sha256 "6548aaf741cf33b3242e221a9f5cea59d8bef092852ec69cf9eed3a234b1cf85"
+  version "1.0.1"
+  sha256 "aa232ca600efcd1b4333c749bf5e68822ed21efa655c91ab9ac2f830993ef6b0"
 
   url "https://github.com/ArronAI007/AILearnHub/releases/download/v#{version}/AILearnHub.zip"
   name "AILearnHub"
