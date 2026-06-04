50d19022b42c8c6c0a0126dd63962a9769716549
贺治东 <16471114+arronai@user.noreply.gitee.com>
2026-06-04 11:35:12 +0800

    v1.0.5: menu button sidebar redesign

--- Casks/ailernhub.rb
+++ Casks/ailernhub.rb
@@ -1,6 +1,6 @@
 cask "ailernhub" do
-  version "1.0.4"
-  sha256 "67edfad013b46982e2a7d25765cedb82c9df764f7b87df8055f0cd42a1a8f83d"
+  version "1.0.5"
+  sha256 "2580ac18fc6cc3dbebf21fbf8f0543581f1e79ed5af8d00da9a62418d82e7e1f"
 
   url "https://raw.githubusercontent.com/ArronAI007/homebrew-tap/main/releases/AILearnHub-#{version}.zip"
   name "AILearnHub"
